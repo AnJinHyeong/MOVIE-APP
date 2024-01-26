06531ca12400102b679f380a26240cbf197a6ad0
안진형 <wlsgud357@naver.com>
2024-01-26 15:51:30 +0900

    영화검색 varcel 최종

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -938,7 +938,13 @@ const searchMovies = async (page)=>{
         store.state.message = "";
     }
     try {
-        const res = await fetch(`https://omdbapi.com?apikey=d85ad9b8&s=${store.state.searchText}&page=${page}`);
+        const res = await fetch("/api/movie", {
+            method: "POST",
+            body: JSON.stringify({
+                title: store.state.searchText,
+                page
+            })
+        });
         const { Search, totalResults, Response, Error } = await res.json();
         if (Response === "True") {
             store.state.movies = [
@@ -958,7 +964,12 @@ const searchMovies = async (page)=>{
 };
 const getMovieDetails = async (id)=>{
     try {
-        const res = await fetch(`https://omdbapi.com?apikey=d85ad9b8&i=${id}&plot=full`);
+        const res = await fetch("/api/movie", {
+            method: "POST",
+            body: JSON.stringify({
+                id
+            })
+        });
         store.state.movie = await res.json();
     } catch (error) {
         console.log("getMovieDetails error:", error);
