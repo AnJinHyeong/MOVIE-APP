135212feeb38d51d52b7c53e6bcea23b0e81d0ef
안진형 <wlsgud357@naver.com>
2024-01-29 16:56:06 +0900

    blog 주소 수정

--- src/store/about.js
+++ src/store/about.js
@@ -4,7 +4,7 @@ export default new Store({
   photo: 'https://heropy.blog/css/images/logo.png',
   name: 'JINMAN / AnJinHyeong',
   email: 'lwwmwn545@gmail.com',
-  blog: 'https://www.naver.com',
+  blog: 'https://velog.io/@jinkong2/posts',
   github: 'https://github.com/AnJinHyeong',
   repository: 'https://github.com/AnJinHyeong/MOVIE-APP',
 })
